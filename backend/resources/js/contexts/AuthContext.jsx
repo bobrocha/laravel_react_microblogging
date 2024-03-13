9a94cae4b344cda4b7d9c7466a3eafec5a81466b
Robert <oxconcise@hotmail.com>
2024-03-13 19:21:28 -0400

    added missing part

--- backend/resources/js/contexts/AuthContext.jsx
+++ backend/resources/js/contexts/AuthContext.jsx
@@ -1,26 +1,61 @@
-
 import {
     createContext,
     useContext,
-    useState
+    useState,
+    useEffect,
 } from 'react';
 
-const AuthContext = createContext(null);
+export const AuthContext = createContext(null);
 
 export function AuthProvider({ children }) {
     const [user, setUser] = useState(null);
 
+
+    useEffect(() => {
+        const user = getLocalStorageUser();
+
+        if(user) {
+            setUser(user);
+        }
+    }, []);
+
     function login(user, callback = () => {}) {
+        setLocalStorageUser(user);
         setUser(user);
         callback();
     }
 
     function logout(callback = () => {}) {
+        destroyLocalStorageUser();
         setUser(null);
         callback();
     }
 
-    const value = { login, logout, user };
+    function isAuthenticated() {
+        if(user) {
+            return user.authenticated;
+        }
+
+        return getLocalStorageUser()?.authenticated ?? false;
+    }
+
+    function getLocalStorageUser() {
+        const user = localStorage.getItem('user');
+
+        return user ? JSON.parse(user) : null
+    }
+
+    function setLocalStorageUser(user) {
+        localStorage.setItem('user', JSON.stringify(user));
+    }
+
+    function destroyLocalStorageUser() {
+        if(getLocalStorageUser()) {
+            localStorage.removeItem('user');
+        }
+    }
+
+    const value = { login, logout, isAuthenticated, user };
 
     return (
         <AuthContext.Provider value={value}>
